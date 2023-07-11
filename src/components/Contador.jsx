4b2b5ce19605c7e68db27fe3cb44034d2528bf96
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-10 22:19:33 -0300

    añadimosBootstrap

---
+++ src/components/Contador.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const Contador = () => {
+  return (
+    <section>
+      <h1>contador</h1>
+      <h2>10</h2>
+      <Button variant="primary"></Button>
+    </section>
+  );
+};
+
+export default Contador;
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,14 @@
+const Titulos = () => {
+  // aqui vamos a pomer la logica
+  return (
+    //   aqui tambien se puede poner logica
+    <section className="text-center">
+      {/* aqui va todo el maquetado */}
+      <h1 className="display-4">proyecto contador reac</h1>
+      <h2 className="dispaly-6">este es un subtitulo</h2>
+      <hr />
+    </section>
+  );
+};
+
+export default Titulos;
